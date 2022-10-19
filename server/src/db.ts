504a5a9b457603fde235115b2a1110b3b6b76f1a
Pierre Genthon <genthon.pierre@gmail.com>
2022-10-19 13:42:12 +0200

    dockerfiles front back + docker compose

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,12 +1,16 @@
-import { DataSource } from 'typeorm';
-import Wilder from './entity/Wilder';
-import Skill from './entity/Skill';
-import Grade from './entity/Grade';
+import { DataSource } from "typeorm";
+import Wilder from "./entity/Wilder";
+import Skill from "./entity/Skill";
+import Grade from "./entity/Grade";
 
 export default new DataSource({
-  type: 'sqlite',
-  database: './wildersdb.sqlite',
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "wilders-book",
   synchronize: true,
   entities: [Wilder, Skill, Grade],
-  logging: ['query', 'error'],
+  logging: ["query", "error"],
 });
