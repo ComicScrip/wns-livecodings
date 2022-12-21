3ee88d075169ee916dfa9e8f0093e3c8129d0923
Pierre Genthon <genthon.pierre@gmail.com>
2022-12-21 11:47:22 +0100

    tmp

--- e2e-tests/src/login.spec.ts
+++ e2e-tests/src/login.spec.ts
@@ -3,6 +3,7 @@ import db from "../../server/src/db";
 import Wilder from "../../server/src/entity/Wilder";
 
 test("can login with correct credentials", async ({ page }) => {
+  await db.initialize();
   await db.getRepository(Wilder).delete({});
   await page.goto("/");
   page.getByPlaceholder("email").type("test@test.com");
