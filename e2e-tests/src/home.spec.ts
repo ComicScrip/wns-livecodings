87c4167f15437fc3976f80816428005cb4ba7ff2
Pierre Genthon <genthon.pierre@gmail.com>
2022-12-22 08:58:44 +0100

    login

--- e2e-tests/src/home.spec.ts
+++ e2e-tests/src/home.spec.ts
@@ -8,7 +8,6 @@ test.beforeEach(clearDB);
 test.afterAll(disconnect);
 
 test("can view wilders in db", async ({ page }) => {
-  await db.getRepository(Wilder).delete({});
   await db
     .getRepository(Wilder)
     .insert([{ name: "Wilder1" }, { name: "Wilder2" }]);
@@ -17,4 +16,11 @@ test("can view wilders in db", async ({ page }) => {
 
   await expect(page.getByTestId("wilder-list")).toContainText("Wilder1");
   await expect(page.getByTestId("wilder-list")).toContainText("Wilder2");
+});
+
+test("can add a wilder", async ({ page }) => {
+  await page.goto("/");
+  await page.getByTestId("newWilderName").type("Dave Lopper");
+  await page.getByRole("button", { name: "+" }).click();
+  await expect(page.getByTestId("wilder-list")).toContainText("Dave Lopper");
 });
