73c81517e5c5d3a183ef7ed6b5af69497ca6a065
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-02 10:06:27 +0100

    integration tests ci

--- integration-tests/src/wilderResolver.test.ts
+++ integration-tests/src/wilderResolver.test.ts
@@ -39,7 +39,7 @@ describe("Wilder resolver", () => {
           mutation: createWilderMutation,
           variables: { data: { name: "" } },
         })
-      ).rejects.toThrow();
+      ).rejects.toThrowErrorMatchingInlineSnapshot();
     });
   });
 
