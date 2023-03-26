343e8ead8c2a16779f6d778528017a195cfc31db
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-26 17:49:48 +0200

    test

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker compose -f docker-compose.staging.yml down && \
     docker compose -f docker-compose.staging.yml pull && \
-    docker compose -f docker-compose.staging.yml up -d --env-file .env.staging;
+    docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
