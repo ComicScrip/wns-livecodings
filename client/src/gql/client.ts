8bbb1a5ddccbd83a3639e6c28eff5f1d833786aa
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-26 14:36:06 +0200

    test

--- client/src/gql/client.ts
+++ client/src/gql/client.ts
@@ -1,5 +1,7 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
+console.log("api url : ", process.env.REACT_APP_GRAPHQL_API_URL);
+
 //https://www.apollographql.com/docs/react/networking/authentication/#cookie
 export default new ApolloClient({
   cache: new InMemoryCache(),
