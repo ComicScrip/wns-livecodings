b4dd87da5a7c69630814522b0f93a4db1d1287dd
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-26 17:47:03 +0200

    test

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker compose -f docker-compose.staging.yml down && \
     docker compose -f docker-compose.staging.yml pull && \
-    GATEWAY_PORT=8001 docker compose -f docker-compose.staging.yml up -d;
+    docker compose -f docker-compose.staging.yml up -d --env-file .env.staging;
