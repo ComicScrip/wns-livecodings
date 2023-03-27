41190a576e7d1e6291a26a0eb56d31ac1476acc4
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-27 13:08:59 +0200

    tmp

--- deploy-main.sh
+++ deploy-main.sh
@@ -1,3 +1,5 @@
-docker compose -f docker-compose.staging.yml down && \
-    docker compose -f docker-compose.staging.yml pull && \
-    docker compose -f docker-compose.staging.yml up -d;
+#!/bin/sh
+git fetch origin && git reset --hard origin/main && git clean -f -d && \
+docker compose -f docker-compose.production.yml down && \
+docker compose -f docker-compose.production.yml pull && \
+docker compose -f docker-compose.production.yml --env-file .env.production up -d;
--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+git fetch origin && git reset --hard origin/staging && git clean -f -d && \
 docker compose -f docker-compose.staging.yml down && \
-    docker compose -f docker-compose.staging.yml pull && \
-    docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
+docker compose -f docker-compose.staging.yml pull && \
+docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
