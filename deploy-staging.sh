ead5a47e2d782dcaf8e687285076c53fffcfcefb
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-29 14:04:08 +0200

    update

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git fetch origin && git reset --hard origin/CD-lc3 && git clean -f -d && \
+git fetch origin && git reset --hard origin/develop && git clean -f -d && \
 docker compose -f docker-compose.staging.yml down && \
 docker compose -f docker-compose.staging.yml pull && \
 docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
