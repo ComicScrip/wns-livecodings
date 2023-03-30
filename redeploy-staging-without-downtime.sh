196d172f0b1e4073a0ec0ee393d9f223324b6d96
Pierre Genthon <genthon.pierre@gmail.com>
2023-03-30 20:46:47 +0200

    test rollout

--- redeploy-staging-without-downtime.sh
+++ redeploy-staging-without-downtime.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
-git fetch origin && git reset --hard origin/develop && git clean -f -d && \
-docker compose -f docker-compose.staging.yml pull && \
-docker rollout -f docker-compose.staging.yml --env-file .env.staging server
-docker rollout -f docker-compose.staging.yml --env-file .env.staging client
+#!/bin/bash
+
+
+#git fetch origin && git reset --hard origin/develop && git clean -f -d && \
+#docker compose -f docker-compose.staging.yml pull && \
+#JWT_PRIVATE_KEY=zeyfgzeigfyzegfyuzegf \
+#NODE_ENV="development" \
+#CORS_ALLOWED_ORIGINS="http://localhost:3000,http://localhost:4000,http://localhost:80,http://192.168.1.7:4000" \
+#SERVER_HOST="localhost" \
+#SERVER_PORT="4000" \
+#DB_HOST=localhost \
+#docker rollout -f docker-compose.staging.yml server
+#docker rollout -f docker-compose.staging.yml --env-file .env.staging client
+
+
+#docker compose -f docker-compose.staging.yml --env-file .env.staging up server --no-deps --scale server=2 --no-recreate
+
