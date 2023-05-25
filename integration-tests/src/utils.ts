58258d16d90143967666dd1c68eecb84ba51542f
Pierre Genthon <genthon.pierre@gmail.com>
2023-05-25 12:21:59 +0200

    test delete wilder mutation with authorization

---
+++ integration-tests/src/utils.ts
@@ -0,0 +1,10 @@
+import db from "../../server/src/db";
+import User from "../../server/src/entity/User";
+import jwt from "jsonwebtoken";
+
+import { env } from "../../server/src/env";
+
+export async function getJWTFor(user: Partial<User>) {
+  if (!user.id) await db.getRepository(User).save(user);
+  return jwt.sign({ userId: user.id }, env.JWT_PRIVATE_KEY);
+}
